6743e0a969b989969c987b48f33e2b79eb66d536
Shinde Jayesh Arun <shindejayesharun@gmail.com>
2021-04-30 00:40:46 +0530

    Initial Commit

---
+++ swiftui_basic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui_basic
+//
+//  Created by Profcyma on 30/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui_basic/swiftui_basicApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_basicApp.swift
+//  swiftui_basic
+//
+//  Created by Profcyma on 30/04/21.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_basicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
